e8a64ef9a83b8d318234f41319239940b132ed50
Justin Lamb <justinlamb@me.com>
2023-11-28 13:27:20 -0800

    header tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'jl-purple' : '#7763b2',
+      },
+    },
+  },
+  plugins: [],
+}
+
